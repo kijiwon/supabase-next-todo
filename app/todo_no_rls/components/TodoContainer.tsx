350d0919e75cf1658b78f68fc2f4bc2530583ec5
kijiwon <kiji1313@gmail.com>
2024-10-10 20:08:15 +0900

    useTodosController 훅 생성

--- app/todo_no_rls/components/TodoContainer.tsx
+++ app/todo_no_rls/components/TodoContainer.tsx
@@ -1,25 +1,11 @@
 "use client";
 
-import {
-  createTodos,
-  deleteTodosHard,
-  deleteTodosSoft,
-  getTodos,
-  getTodosById,
-  getTodosBySearch,
-  updateTodos,
-} from "@/apis/todos-no-rls";
-import React, { useEffect } from "react";
+import useTodosController from "../hooks/useTodosController";
 
 const TodoContainer = () => {
-  useEffect(() => {
-    // getTodos();
-    // getTodosById(4);
-    // getTodosBySearch("공부");
-    // createTodos("createTodos");
-    // updateTodos(5, "todos 업데이트!");
-    deleteTodosHard(5);
-  }, []);
+  const { loading, todos } = useTodosController();
+  console.log(">>loading", loading);
+  console.log(">>todos", todos);
 
   return <div>TodoContainer</div>;
 };
