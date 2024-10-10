207b37fb72579892336434d888d1db7ebf0b7f94
kijiwon <kiji1313@gmail.com>
2024-10-10 15:03:08 +0900

    createBrowserClient함수 생성

---
+++ lib/client/supabse.ts
@@ -0,0 +1,11 @@
+// 웹 페이지 안에서 supabase의 REST API를 호출할 때 사용하는 함수
+import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "@/database.types";
+
+export const createSupabaseBrowserClient = () => {
+  createBrowserClient<Database>(
+    // supabase url과 supabase anon key를 전달
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
