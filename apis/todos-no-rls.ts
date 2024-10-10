149949fcb97508f631284c5b9db437be0a890b8a
kijiwon <kiji1313@gmail.com>
2024-10-10 16:23:09 +0900

    검색어가 포함된 데이터 요청

--- apis/todos-no-rls.ts
+++ apis/todos-no-rls.ts
@@ -29,3 +29,18 @@ export const getTodosById = async (id: number) => {
 
   return result.data;
 };
+
+// todoList 가져오기 + search
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    // 검색어가 포함된 content 가져오기
+    // ilike -> terms의 대소문자 구분하지 않음
+    .ilike("content", `%${terms}%`)
+    .order("id", { ascending: false });
+
+  return result.data;
+};
--- app/todo_no_rls/components/TodoContainer.tsx
+++ app/todo_no_rls/components/TodoContainer.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { getTodos, getTodosById } from "@/apis/todos-no-rls";
+import { getTodos, getTodosById, getTodosBySearch } from "@/apis/todos-no-rls";
 import React, { useEffect } from "react";
 
 const TodoContainer = () => {
   useEffect(() => {
     // getTodos();
-    getTodosById(4);
+    // getTodosById(4);
+    getTodosBySearch("공부");
   }, []);
 
   return <div>TodoContainer</div>;
