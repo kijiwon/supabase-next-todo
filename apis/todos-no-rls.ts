25e09884da1d7763d9aaedcb683ac5df9fbb5174
kijiwon <kiji1313@gmail.com>
2024-10-10 16:17:04 +0900

    id를 전달해 데이터 요청

--- apis/todos-no-rls.ts
+++ apis/todos-no-rls.ts
@@ -16,3 +16,16 @@ export const getTodos = async () => {
 
   return result.data;
 };
+
+// todoList 가져오기 + by Id
+export const getTodosById = async (id: number) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    // 동일한 id만 가져오기
+    .eq("id", id);
+
+  return result.data;
+};
