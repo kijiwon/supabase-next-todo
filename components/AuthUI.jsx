e4a1131a514d23c856f32c4592bfc498db83ec61
kijiwon <kiji1313@gmail.com>
2024-10-17 00:07:58 +0900

    auth ui 설정

---
+++ components/AuthUI.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { createSupabaseBrowserClient } from "@/lib/client/supabse";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const AuthUI = () => {
+  const supabase = createSupabaseBrowserClient();
+  return (
+    <section className=" w-full">
+      <div className=" max-w-[500px] mx-auto">
+        <Auth
+          supabaseClient={supabase}
+          appearance={{
+            theme: ThemeSupa,
+          }}
+          onlyThirdPartyProviders // -> 이메일 로그인은 사용하지 않음
+          providers={["google", "github"]}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default AuthUI;
