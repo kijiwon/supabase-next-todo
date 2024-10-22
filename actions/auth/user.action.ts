b3bd023654ff6e179d2da6711c275e4dbd036208
kijiwon <kiji1313@gmail.com>
2024-10-22 20:36:14 +0900

    getUser 변수 추가

--- actions/auth/user.action.ts
+++ actions/auth/user.action.ts
@@ -1,7 +1,10 @@
 import { createServerSideClient } from "@/lib/supabase";
 
-export const getUser = async () => {
-  const supabase = await createServerSideClient();
+// serverComponent 인자로 받기
+// 서버 액션의 경우 서버 사이드 랜더링 도중 호출될 수 있음 -> 서버는 쿠키 조작이 불가능
+// 이러한 환경을 분기처리하기 위해 serverComponent 변수를 추가
+export const getUser = async ({ serverComponent = false }) => {
+  const supabase = await createServerSideClient(serverComponent);
   const user = await supabase.auth.getUser();
   return user?.data?.user;
 };
